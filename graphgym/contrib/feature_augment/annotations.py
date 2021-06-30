2f1c74743e313f3e4ec28f80da459032b0f06a00
Benjamin Moser <benjamin.2.moser@uni-konstanz.de>
2021-06-30 12:55:05 +0200

    basic implementation of species class feature augment
    ↝ [[compute feature augments]]

---
+++ graphgym/contrib/feature_augment/annotations.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import numpy as np
+from graphgym.register import register_feature_augment
+
+def node_class_onehot_func(graph, **kwargs):
+    return onehot_attr(graph, "class")
+register_feature_augment('node_class_onehot', node_class_onehot_func)
+
+def onehot_attr(graph, key):
+    attributes = nx.get_node_attributes(graph.G, key)
+    possible_values = list(set(attributes.values()))
+    def onehot_enc(value, possible_values):
+        f = np.zeros(len(possible_values))
+        f[possible_values.index(value)] = 1  # probably faster to use a dict to get the int
+        return f
+    r = [
+        onehot_enc(value, possible_values) for value in attributes.values()
+    ]
+    assert len(r) == len(graph.G.nodes)
+    return r
