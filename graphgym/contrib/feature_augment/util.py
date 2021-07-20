dcf41f: fix normalisation of features computed on bipartite projection

graphgym/contrib/feature_augment/util.py
@@ -1,6 +1,7 @@
 import statistics
 
 import networkx
+import numpy as np
 import torch
 
 import deepsnap
@@ -131,13 +132,36 @@ def get_bip_proj_cached(graph):
         assert bipartite.is_bipartite_node_set(graph.G, non_rxn_nodes)
         bipartite_projection = bipartite.projected_graph(graph.G,
                                                          non_rxn_nodes)  # connected if common neighbour in rxn_nodes
-        # should not contain isolated nodes if original graph didnt... but occurs anyways?
-        # no_deg_nodes = [node for (node, degree) in bipartite_projection.degree if degree == 0]
-        # bipartite_projection.remove_nodes_from(no_deg_nodes)
         dsG = deepsnap.graph.Graph(bipartite_projection,
                                    # avoid updating internal tensor repr
                                    edge_label_index=[],
                                    node_label_index=[]
                                    )
+
+        # selected nodes in original graph
+        # i.e. ids of nodes that we want to consider in this split
+        node_ids, node_idx, _ = tens_intersect(graph['node_label_index'], torch.tensor(non_rxn_nodes))
+        dsG['node_label_index'] = node_idx
         graph['bipartite_projection'] = dsG
     return graph['bipartite_projection']
+
+
+def tens_intersect(x: torch.Tensor, y: torch.Tensor):
+    intersect, x_ind, y_ind = np.intersect1d(x.cpu().numpy(), y.cpu().numpy(), return_indices=True)
+    return torch.tensor(intersect), torch.tensor(x_ind), torch.tensor(y_ind)
+
+
+def collect_feature_augment(batch, node_index):
+    """
+    Concat information from feature augments into node_feature tensor. Same as `Processing` module does in GNN models.
+    Additionally takes a node_index mask of nodes to consider.
+    :param batch:
+    :param node_index:
+    :return:
+    """
+    dim_dict = {name: dim
+                for name, dim in zip(cfg.dataset.augment_feature,
+                                     cfg.dataset.augment_feature_dims)}
+    return torch.cat(
+        [batch[name][node_index].float() for name in dim_dict],
+        dim=1)

graphgym/contrib/train/SVM.py
@@ -1,27 +1,10 @@
-import numpy as np
 import torch
 from graphgym.config import cfg
-from graphgym.contrib.feature_augment.util import get_non_rxn_nodes
+from graphgym.contrib.feature_augment.util import get_non_rxn_nodes, tens_intersect, collect_feature_augment
 from graphgym.register import register_train
 # Do not train a neural network but invoke some other algorithm
 # e.g. baseline classifier such as SVM
 from sklearn import svm
-
-
-def collect_feature_augment(batch, node_index):
-    """
-    Concat information from feature augments into node_feature tensor. Same as `Processing` module does in GNN models.
-    Additionally takes a node_index mask of nodes to consider.
-    :param batch:
-    :param node_index:
-    :return:
-    """
-    dim_dict = {name: dim
-                for name, dim in zip(cfg.dataset.augment_feature,
-                                     cfg.dataset.augment_feature_dims)}
-    return torch.cat(
-        [batch[name][node_index].float() for name in dim_dict],
-        dim=1)
 
 
 def logical_and_pad(a, b):
@@ -43,11 +26,6 @@ def logical_and_pad(a, b):
     padding = torch.zeros(diff)
     padded = torch.cat([smaller, padding], dim=0)
     return torch.logical_and(greater, padded)
-
-
-def tens_intersect(x: torch.Tensor, y: torch.Tensor):
-    intersect, x_ind, y_ind = np.intersect1d(x.cpu().numpy(), y.cpu().numpy(), return_indices=True)
-    return torch.tensor(intersect), torch.tensor(x_ind), torch.tensor(y_ind)
 
 
 def run_svm(loggers, loaders, model, optimizer, scheduler):
@@ -80,6 +58,8 @@ def run_svm(loggers, loaders, model, optimizer, scheduler):
     # When selecting non-rxn nodes from node_label_index we need to do the same
     #   for node_label. We remember the indices we picked from node_label_index
     #   and use these to index into node_label.
+    # TODO instead, use graph['bipartite_projection']['node_label_index'] here
+    #   as computed in get_bip_proj_cached?
     train_label_index, picked_test, _ = tens_intersect(batch_train.node_label_index, non_rxn_index)
     test_label_index, picked_train, _ = tens_intersect(batch_test.node_label_index, non_rxn_index)
     X_train = X_augm[train_label_index]  # this is essentially our feature matrix

graphgym/contrib/transform/normalize.py
@@ -25,10 +25,17 @@ def normalize_stateful(graph, scalers=None):
         # apply previously initialised scaler
         # only consider features in current split, i.e. those indicated by label_index
         # note that technically the entire tensor of size n is present but we only
-        # touch those corresponding to the current split
+        # touch those corresponding to the current split.
+
+        if key.endswith('_projection'):
+            # label_index corresponds to the entire graph (not bipartite projection)
+            #   this is a problem if feature (in graph_key) are of smaller shape (if computed on bipartite projection)
+            label_index = graph['bipartite_projection']['node_label_index']
+        else:
+            label_index = graph.node_label_index
         original = graph[key][label_index]
         transformed = scaler.transform(original)
-        graph[key][label_index] = torch.from_numpy(transformed).to(torch.float)
+        graph[key][label_index] = torch.from_numpy(transformed).to(torch.float32)
 
 
 def fit_normalizers(dataset) -> dict: