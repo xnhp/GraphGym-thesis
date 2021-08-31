86fe3a2fa72b72a6858bf61d06a6aa82e6cf9667
Benjamin Moser <benjamin.2.moser@uni-konstanz.de>
2021-08-31 15:07:55 +0200

    6f8c67: add weighted BCE loss

---
+++ graphgym/contrib/loss/bce_weighted.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+from graphgym.register import register_loss
+
+from graphgym.config import cfg
+
+
+def loss_bce_weighted(pred, true):
+    if cfg.model.loss_fun == 'cross_entropy_weighted':
+        pos_weight = 10
+        bce_loss = nn.BCEWithLogitsLoss(
+            reduction=cfg.model.size_average,   # same as in original approach
+            pos_weight=torch.tensor([pos_weight]).to(torch.device(cfg.device))
+        )
+        true = true.float()
+        return bce_loss(pred, true), torch.sigmoid(pred)
+
+
+register_loss('cross_entropy_weighted', loss_bce_weighted)
